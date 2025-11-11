3c3f185508c7ddf51f3060f421608ec49983e8a4
SATHISH B <sathishb2310@gmail.com>
2025-11-11 21:16:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 //complete this code
-class Person {}
+class Person {
+	Person(name,age){
+		this.name = name;
+		this.age = age;
+	}
+	getter(){
+		return this.name;
+	}
+	setter(){
+		this.age = age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	Student(name,age){
+		this.name = name;
+		this.age = age;
+	}
+	study(){
+		return this.name+" is studying "
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	Teacher(name,age){
+		this.name = name;
+		this.age = age;
+	}
+	teach(){
+		return this.name+" is teaching"
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
